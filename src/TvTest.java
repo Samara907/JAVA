20824c7d7ce77c53e008bb7cdcd017d758e7b98f
samara907 <mabin.kjy@gmail.com>
2016-06-01 11:34:50 +0900

    Objects and instances

---
+++ src/TvTest.java
@@ -0,0 +1,21 @@
+
+class Tv {
+	// Tv's Property(member variable)
+	String color;
+	boolean power;
+	int channel;
+	
+	// Tv's Function(method)
+	void power () { power = !power; }
+	void channelUp () { ++channel; }
+	void channelDown () { --channel; }
+}
+public class TvTest {
+	public static void main(String args[]) {
+		Tv t;
+		t = new Tv();
+		t.channel = 7;
+		t.channelDown();
+		System.out.println("channel's value is " + t.channel);
+	}
+}
